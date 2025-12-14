17c14da9749d9b7f642b18eca418d83978e20cd9
candrabudi <bagus.candrabudi@gmail.com>
2025-12-14 23:53:18 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,44 +2,45 @@
 
 /** @var Laravel\Lumen\Routing\Router $router */
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->post('/login', 'AuthController@login');
+    $router->post('login', 'AuthController@login');
 
     $router->group(['middleware' => 'auth'], function () use ($router) {
-        $router->post('/logout', 'AuthController@logout');
+        $router->post('logout', 'AuthController@logout');
 
-        $router->get('/profile', 'ProfileController@show');
-        $router->put('/profile', 'ProfileController@update');
-        $router->post('/change-password', 'ProfileController@changePassword');
+        $router->group(['prefix' => 'profile'], function () use ($router) {
+            $router->get('/', 'ProfileController@show');
+            $router->put('/', 'ProfileController@update');
+            $router->post('change-password', 'ProfileController@changePassword');
+        });
 
-        $router->get('/group-fields', 'GroupFieldController@index');
-        $router->get('/group-fields/{id}', 'GroupFieldController@show');
-        $router->post('/group-fields', 'GroupFieldController@store');
-        $router->put('/group-fields/{id}', 'GroupFieldController@update');
-        $router->delete('/group-fields/{id}', 'GroupFieldController@destroy');
+        $router->group(['prefix' => 'group-fields'], function () use ($router) {
+            $router->get('/', 'GroupFieldController@index');
+            $router->get('{id}', 'GroupFieldController@show');
+            $router->post('/', 'GroupFieldController@store');
+            $router->put('{id}', 'GroupFieldController@update');
+            $router->delete('{id}', 'GroupFieldController@destroy');
+        });
 
-        $router->get('/cpcl-documents', 'CpclDocumentController@index');
-        $router->get('/cpcl-documents/{id}', 'CpclDocumentController@show');
-        $router->post('/cpcl-documents', 'CpclDocumentController@store');
-        $router->put('/cpcl-documents/{id}', 'CpclDocumentController@update');
-        $router->delete('/cpcl-documents/{id}', 'CpclDocumentController@destroy');
-        $router->put('/cpcl-documents/{id}/status', 'CpclDocumentController@updateStatus');
+        $router->group(['prefix' => 'cpcl-documents'], function () use ($router) {
+            $router->get('/', 'CpclDocumentController@index');
+            $router->get('{id}', 'CpclDocumentController@show');
+            $router->post('/', 'CpclDocumentController@store');
+            $router->put('{id}', 'CpclDocumentController@update');
+            $router->delete('{id}', 'CpclDocumentController@destroy');
+            $router->put('{id}/status', 'CpclDocumentController@updateStatus');
+        });
 
         $router->group(['prefix' => 'cpcl-applicants'], function () use ($router) {
-            $router->get('/', ['uses' => 'CpclApplicantController@index']);
-            $router->get('{id}', ['uses' => 'CpclApplicantController@show']);
-            $router->post('/', ['uses' => 'CpclApplicantController@store']);
-            $router->put('{id}', ['uses' => 'CpclApplicantController@update']);
-            $router->delete('{id}', ['uses' => 'CpclApplicantController@destroy']);
+            $router->get('/', 'CpclApplicantController@index');
+            $router->get('{id}', 'CpclApplicantController@show');
+            $router->post('/', 'CpclApplicantController@store');
+            $router->put('{id}', 'CpclApplicantController@update');
+            $router->delete('{id}', 'CpclApplicantController@destroy');
         });
 
         $router->group(['prefix' => 'cpcl-answers'], function () use ($router) {
-            $router->post('/', [
-                'uses' => 'CpclAnswerController@store',
-            ]);
-
-            $router->get('/{cpclDocumentId}', [
-                'uses' => 'CpclAnswerController@show',
-            ]);
+            $router->post('/', 'CpclAnswerController@store');
+            $router->get('{cpcl_document_id}', 'CpclAnswerController@show');
         });
     });
 });

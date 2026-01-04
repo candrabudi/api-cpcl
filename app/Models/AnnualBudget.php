0024d79260147b2c21cefb7c7256dfca54ce6c18
candrabudi <bagus.candrabudi@gmail.com>
2026-01-04 19:02:56 +0700

    improve

--- app/Models/AnnualBudget.php
+++ app/Models/AnnualBudget.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class AnnualBudget extends Model
@@ -11,22 +9,13 @@ class AnnualBudget extends Model
     protected $fillable = [
         'budget_year',
         'total_budget',
-        'used_budget',
-        'remaining_budget',
     ];
 
     protected $appends = ['used_budget', 'remaining_budget'];
 
-    public function procurements()
-    {
-        return $this->hasMany(Procurement::class);
-    }
-
     public function getUsedBudgetAttribute()
     {
-        return $this->procurements()->with('items')->get()->sum(function ($p) {
-            return $p->items->sum('total_price');
-        });
+        return 0;
     }
 
     public function getRemainingBudgetAttribute()
